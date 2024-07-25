c0993b10bbbeb319a287c0acb5674243d5b19f8b
r2dd <david.dams@tutanota.com>
2024-07-25 16:40:56 +0200

    TEST(overlap): cgf derivative

--- tests/test_overlap.py
+++ tests/test_overlap.py
@@ -96,7 +96,7 @@ def test_contracted(tolerance =  1e-10):
     assert abs(overlap_ref12 - overlap12) < tolerance
     assert abs(overlap_ref22 - overlap22) < tolerance
 
-def test_derivative():    
+def test_derivative( tolerance = 1e-4):    
     a1, l1, m1, n1, x1, y1, z1 =  0.2, 4, 1, 0, 0.2, 0.3, 0.1
     a2, l2, m2, n2, x2, y2, z2 =  0.1, 1, 1, 2, 10., 0.1, 0.5
     
@@ -107,9 +107,50 @@ def test_derivative():
     func = lambda x : overlap(gaussian1, gaussian2.at[:3].set(x), l_max)
     grad = jax.jit(jax.jacrev(func))
     g = grad( jnp.array([x2, y2, z2]) )
+    
+    eps = 1e-8
+    num = (func(jnp.array([x2 + eps, y2, z2])) - func(jnp.array([x2, y2, z2]))) / eps
 
+    print(abs(g[0] - num))
+    assert abs(g[0] - num) < tolerance
+
+
+def test_derivative_contracted(tolerance =  1e-4):
+    """test contracted gaussian overlaps (i.e. primitive overlaps multiplied by coefficients and normalization factors)"""
+
+    gaussians = [
+            [0.2, 0.3, 0.1, 4, 1, 0, 0.2], 
+            [0.2, 0.3, 0.1, 3, 2, 1, 0.3], 
+            [0.2, 0.3, 0.1, 2, 3, 2, 0.4],
+            
+            [0.5, 0.6, 0.4, 4, 1, 0, 0.5], 
+            [0.5, 0.6, 0.4, 3, 2, 1, 0.6], 
+            [0.5, 0.6, 0.4, 2, 3, 2, 0.7] 
+    ]
+
+    coeffs = [
+        [0.1, 3., 1., 0., 0., 0.],
+        [0., 0., 0., 0.4, 2., 7.],
+    ]    
+    
+    a2, l2, m2, n2, x2, y2, z2 =  0.1, 1, 1, 2, 10., 0.1, 0.5
+
+    gs, cs = jnp.array(gaussians), jnp.array(coeffs)
+    
+    l_max = int(jnp.max(gs[:, 3:6]) + 1)
+    func = jax.jit(lambda x : promote_one(lambda g1, g2 : overlap(g1.at[:3].set(x), g2, l_max))(cs[0, :3], cs[1, 3:], gs[:3], gs[3:]))
+    grad = jax.jit(jax.jacrev(func))    
+    g = grad(jnp.array([x2, y2, z2]))
+
+    eps = 1e-8
+    num = (func(jnp.array([x2 + eps, y2, z2])) - func(jnp.array([x2, y2, z2]))) / eps
+    
+    print(abs(g[0] - num))
+    assert abs(g[0] - num) < tolerance
+    
 if __name__ == '__main__':
     test_primitive()
     test_contracted()
     test_derivative()
+    test_derivative_contracted()
     
