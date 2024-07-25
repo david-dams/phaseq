66b4dd8c9594d5b3a36f7138af72457a192f80b9
r2dd <david.dams@tutanota.com>
2024-07-25 16:15:31 +0200

    TEST(interaction): cgf

--- tests/test_interaction.py
+++ tests/test_interaction.py
@@ -49,8 +49,58 @@ def test_primitive(tolerance = 1e-10):
             
 def test_contracted(tolerance =  1e-10):
     """test contracted gaussian interactions (i.e. primitive interactions multiplied by coefficients and normalization factors)"""
-    pass
+    gaussians = [
+            [0.2, 0.3, 0.1, 4, 1, 0, 0.2], 
+            [0.2, 0.3, 0.1, 3, 2, 1, 0.3], 
+            [0.2, 0.3, 0.1, 2, 3, 2, 0.4],
+            
+            [0.5, 0.6, 0.4, 4, 1, 0, 0.5], 
+            [0.5, 0.6, 0.4, 3, 2, 1, 0.6], 
+            [0.5, 0.6, 0.4, 2, 3, 2, 0.7] 
+    ]
 
+    coeffs = [
+        [0.1, 3., 1.,],
+        [0.4, 2., 7.],
+        [0.3, 4., 0.1,],
+        [0.7, 1., 2.],
+    ]    
+    
+    gs, cs = jnp.array(gaussians), jnp.array(coeffs)
+    
+    l_max = 2*int(jnp.max(gs[:, 3:6])) + 2
+    
+    func = jax.jit(promote_two(lambda g1, g2, g3, g4 : interaction(g1, g2, g3, g4, l_max)))
+    
+    val = func(cs[0], cs[1], cs[2], cs[3], gs[:3], gs[3:], gs[:3], gs[3:])    
+
+    integrator = PyQInt()
+    
+    cgf1 = cgf(gaussians[0][:3])
+    cgf1.add_gto(coeffs[0][0], gaussians[0][-1], *(gaussians[0][3:6]) )
+    cgf1.add_gto(coeffs[0][1], gaussians[1][-1], *(gaussians[1][3:6]) )
+    cgf1.add_gto(coeffs[0][2], gaussians[2][-1], *(gaussians[2][3:6]) )
+    
+    cgf2 = cgf(gaussians[3][:3])
+    cgf2.add_gto(coeffs[1][0], gaussians[3][-1], *(gaussians[3][3:6]) )
+    cgf2.add_gto(coeffs[1][1], gaussians[4][-1], *(gaussians[4][3:6]) )
+    cgf2.add_gto(coeffs[1][2], gaussians[5][-1], *(gaussians[5][3:6]) )    
+    
+    cgf3 = cgf(gaussians[0][:3])
+    cgf3.add_gto(coeffs[2][0], gaussians[0][-1], *(gaussians[0][3:6]) )
+    cgf3.add_gto(coeffs[2][1], gaussians[1][-1], *(gaussians[1][3:6]) )
+    cgf3.add_gto(coeffs[2][2], gaussians[2][-1], *(gaussians[2][3:6]) )
+    
+    cgf4 = cgf(gaussians[3][:3])
+    cgf4.add_gto(coeffs[3][0], gaussians[3][-1], *(gaussians[3][3:6]) )
+    cgf4.add_gto(coeffs[3][1], gaussians[4][-1], *(gaussians[4][3:6]) )
+    cgf4.add_gto(coeffs[3][2], gaussians[5][-1], *(gaussians[5][3:6]) )    
+
+    ref = integrator.repulsion(cgf1, cgf2, cgf3, cgf4)
+
+    print(abs(val - ref))
+    assert abs(val - ref) < tolerance
+    
 def test_derivative(tolerance = 1e-1):    
     a1, l1, m1, n1, x1, y1, z1 =  0.2, 4, 1, 2, 0.2, 0.3, 0.1
     a2, l2, m2, n2, x2, y2, z2 =  0.1, 1, 1, 0, 10., 0.1, 0.5
@@ -67,7 +117,7 @@ def test_derivative(tolerance = 1e-1):
     gs = jnp.array(gaussians)
     l_max = 2*int(jnp.max(gs[:, 3:6])) + 2
     func = lambda x : interaction(gs[0], gs[1].at[:3].set(x), gs[2], gs[3], l_max)
-    grad = jax.jit(jax.grad(func))
+    grad = jax.jit(jax.jacrev(func))
     g = grad( jnp.array([x2, y2, z2]) )
 
     eps = 1e-8
@@ -76,5 +126,6 @@ def test_derivative(tolerance = 1e-1):
     assert abs(g[0] - num) < tolerance
 
 if __name__ == '__main__':
-    test_primitive()
-    test_derivative()
+    # test_primitive()
+    # test_derivative()
+    test_contracted()
