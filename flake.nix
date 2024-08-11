0501e1244d2448d47286a18c19faa3596d164884
David Dams <david.dams@tutanota.com>
2024-08-11 17:03:08 +0200

    FIX: flake

--- flake.nix
+++ flake.nix
@@ -1,37 +1,25 @@
 {
-  description = "phaseQ";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ pkgs.python312Full pkgs.python312Packages.pip ];
-      };
-
-      packages.default = pkgs.stdenv.mkDerivation {
-        pname = "phaseQ";
-        version = "0.1";
-
-        src = ./.;
-
-        buildInputs = [ pkgs.python312Full pkgs.python312Packages.pip ];
-
-        buildPhase = ''
-        pip install -e .
-        '';
-
-        meta = with pkgs.lib; {
-          description = "";
-          license = licenses.mit;
-        };
-      };
-    });
+    let
+      pythonVersion = "python312";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pythonPackages = pkgs.python312Packages;
+      in
+        {                          
+          devShells.default = pkgs.mkShell {
+            name = "zum kotzen";
+            venvDir = "./.venv";
+            packages = [ pythonPackages.python
+                         pythonPackages.venvShellHook
+                         pythonPackages.numpy
+                         pythonPackages.jax
+                         pythonPackages.jaxlib ];
+          };
+        });
 }
